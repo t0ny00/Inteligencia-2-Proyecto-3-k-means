673389cdaca388f9ef2b12e4633573b22a4c7f84
t0ny00 <tonylaeja@gmail.com>
2017-03-16 18:22:52 -0400

    Added compressed images

--- compressImage.py
+++ compressImage.py
@@ -5,13 +5,13 @@ from cluster import Cluster,kmeans,compress,createClusters
 
 if __name__ == '__main__':
 
-    k = 8
+    k = 128
     number_attributes = 3
     file_name = 'flor.png'
     image = misc.imread(file_name)
     x = image.reshape((image.shape[0] * image.shape[1], 3))
     clusters = createClusters(k, number_attributes, 0, 255)
-    kmeans(clusters, x, 0.001,8)
+    kmeans(clusters, x, 0.1,5)
     compress(clusters, x)
     x = x.reshape(image.shape)
     im = Image.fromarray(x)
