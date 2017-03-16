7c2ec1ded8da17efec5607917cf8886799cdd7e4
t0ny00 <tonylaeja@gmail.com>
2017-03-16 18:45:31 -0400

    Code refactoring and commenting

--- compressImage.py
+++ compressImage.py
@@ -7,13 +7,25 @@ if __name__ == '__main__':
 
     k = 128
     number_attributes = 3
+    threshold = 0.1
+    max_iter = 5
     file_name = 'flor.png'
+
+    # Load image
     image = misc.imread(file_name)
+
+    # Transform pixel matrix into a one dimensional vector
     x = image.reshape((image.shape[0] * image.shape[1], 3))
+
     clusters = createClusters(k, number_attributes, 0, 255)
-    kmeans(clusters, x, 0.1,5)
+    kmeans(clusters, x, threshold,max_iter)
+
+    # Change each pixel color by that of its centroid
     compress(clusters, x)
+
+    # Restore pixel matrix
     x = x.reshape(image.shape)
+
     im = Image.fromarray(x)
     out_file = file_name[:-4]
     out_file += "_" + str(k) + ".png"
