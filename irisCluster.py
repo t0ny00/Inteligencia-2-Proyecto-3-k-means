f8f810dfd1a80345c855099b667071ad0701baaa
Carlos <carlosrmd94@gmail.com>
2017-03-16 20:48:26 -0400

    graficas con indices

--- irisCluster.py
+++ irisCluster.py
@@ -1,8 +1,10 @@
 import numpy as np
+from collections import OrderedDict
 from cluster import Cluster,kmeans,compress,createClusters
 import os.path
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
+import copy
 
 colors = ['b','g','r','c','m']
 file = open("iris-clean.txt",'r')
@@ -25,7 +27,7 @@ def splitData(data,percentage,output_num):
     return data_train,data_test
 
 if __name__ == '__main__':
-    k = 4
+    k = 3
     number_attributes = 4
     data = np.loadtxt("iris-clean.txt", delimiter=",")
     x, junk = splitData(data, 100, 1)
@@ -38,25 +40,78 @@ if __name__ == '__main__':
             if elem.emptyIndex():
                 ready = False
 
+
+    nclusters = len(clusters)
     
-    print("Number of clusters " + str(len(clusters)))
+    print("Number of clusters " + str(nclusters))
     for j in range(2):
-        for i in range(len(clusters)):
+        patches = []
+        names   = []
+        legend_labeler = [True,True,True,True]
+        plt.title("Cluster distribution for Iris with " + str(k) + " clusters part " + str(j+1))
+        for i in range(nclusters):
+            patches.append(mpatches.Patch(color=colors[i]))
+            names.append('Cluster '+str(i+1))
+        for i in range(nclusters):
             print("Cluster "+str(i+1))
             print(clusters[i])
             centroid = clusters[i].getCentroid()
             indexes = clusters[i].getPointsIndex()
-            plt.plot([centroid[0+j*2]],[centroid[1+j*2]],colors[i]+'X')
+            if legend_labeler[0]:
+                centroid_object = plt.scatter(centroid[0+j*2],centroid[1+j*2],c=colors[i],marker='X')
+                co_copy = copy.copy(centroid_object)
+                co_copy.set_facecolor('k')
+                patches.append(co_copy)
+                names.append('Centroid')
+                legend_labeler[0] = False
+            else:
+                plt.plot([centroid[0+j*2]],[centroid[1+j*2]],colors[i]+'X')
             for elem in indexes:
                 marker = ''
+                index_label = ''
+                act = data[elem]
                 if elem < 50:
                     marker = '*'
+                    index_label = "iris setosa element"
+                    if legend_labeler[1]:
+                        obj = plt.scatter(act[0+j*2],act[1+j*2],c=colors[i],marker=marker)
+                        obj_copy = copy.copy(obj)
+                        obj_copy.set_facecolor('k')
+                        patches.append(obj_copy)
+                        names.append(index_label)
+                        legend_labeler[1] = False
+                    else:
+                        plt.scatter(act[0+j*2],act[1+j*2],c=colors[i],marker=marker)
                 elif elem >= 50 and elem < 100:
                     marker = 'D'
+                    index_label = "iris versicolor element"
+                    if legend_labeler[2]:
+                        obj = plt.scatter(act[0+j*2],act[1+j*2],c=colors[i],marker=marker)
+                        obj_copy = copy.copy(obj)
+                        obj_copy.set_facecolor('k')
+                        patches.append(obj_copy)
+                        names.append(index_label)
+                        legend_labeler[2] = False
+                    else:
+                        plt.scatter(act[0+j*2],act[1+j*2],c=colors[i],marker=marker)
                 elif elem >= 100:
                     marker = '^'
-                act = data[elem]
-                plt.plot([act[0+j*2]],[act[1+j*2]],colors[i]+marker)
+                    index_label = "iris virginica element"
+                    if legend_labeler[3]:
+                        obj = plt.scatter(act[0+j*2],act[1+j*2],c=colors[i],marker=marker)
+                        obj_copy = copy.copy(obj)
+                        obj_copy.set_facecolor('k')
+                        patches.append(obj_copy)
+                        names.append(index_label)
+                        legend_labeler[3] = False
+                    else:
+                        plt.scatter(act[0+j*2],act[1+j*2],c=colors[i],marker=marker)
+                
+                
+                
+        handles,labels = plt.gca().get_legend_handles_labels()
+        by_label = OrderedDict(zip(labels, handles))
+        plt.legend(patches,names,loc="lower right",scatterpoints=1, fontsize=6)
         plt.savefig(os.path.splitext(os.path.basename("k" + str(k) + "-" + str(j+1)))[0] + '.png')
         plt.clf()
     #plt.show()
